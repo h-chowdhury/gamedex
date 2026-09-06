e03b3a1fe94e595d82441d8693ab74f09f08c785
Humayra Chowdhury <hc1415926@gmail.com>
2026-09-06 17:59:46 +0100

    Updated default port number in app.js

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ dotenv.config();
 const app = express();
 const router = express.Router();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 const mongoDBURL = process.env.DB_URL || 'mongodb://127.0.0.1:27017/gamedex';
 const rawgAPIkey = process.env.RAWG_API_KEY;
 
